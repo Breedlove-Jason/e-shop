797b899b63e74f4a839705f283374bbb4dafebb4
Breedlove-Jason <j@jasonbreedlove.com>
2024-05-27 15:08:27 -0700

    product props, new products

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -3,6 +3,7 @@ import "./home.styles.css";
 import Product from "../product/product.component";
 
 const Home = () => {
+  let product_id = Math.random() * 1000000000000000000;
   return (
     <div>
       <div className="home">
@@ -15,16 +16,76 @@ const Home = () => {
             alt={"home"}
           />
           <div className="home__row">
-            <Product />
-            <Product />
+            <Product
+              id={product_id}
+              title={
+                "Tablet Android Tablets, 8 inch Tablet 2GB RAM, 32GB ROM Support 512GB Expand Computer Tablet PC, Quad-CoreProcessor, IPS Touch Screen, 2+5MP Dual Camera, 4300mah Battery, Wifi, (Black)"
+              }
+              price={47}
+              rating={3}
+              image={
+                "https://m.media-amazon.com/images/I/41BZugek34L._MCnd_AC_.jpg"
+              }
+            />
+            <Product
+              id={product_id}
+              title={
+                "Apple 2023 MacBook Pro Laptop M2 Pro chip with 12‑core CPU and 19‑core GPU: 16.2-inch Liquid Retina XDR Display, 16GB Unified Memory, 512GB SSD Storage. Works with iPhone/iPad; Space Gray"
+              }
+              price={1998}
+              rating={5}
+              image={
+                "https://m.media-amazon.com/images/I/61fd2oCrvyL._AC_UY218_.jpg"
+              }
+            />
           </div>
           <div className="home__row">
-            <Product />
-            <Product />
-            <Product />
+            <Product
+              id={product_id}
+              title={
+                "Apple iPhone 13 Pro Max, 256GB, Graphite - Fully Unlocked (Renewed)"
+              }
+              price={274}
+              rating={4}
+              image={
+                "https://m.media-amazon.com/images/I/513NI5xpYjL._AC_UL320_.jpg"
+              }
+            />
+            <Product
+              id={product_id}
+              title={
+                "Razer Huntsman V2 Optical Gaming Keyboard: Fastest Linear Optical Switches Gen-2 w/ Sound Dampeners & 8000Hz Polling Rate - Doubleshot PBT Keycaps - Dedicated Media Keys & Dial - Ergonomic Wrist Rest"
+              }
+              price={129}
+              rating={4}
+              image={
+                "https://m.media-amazon.com/images/I/71xiaUDxklL._AC_UY218_.jpg"
+              }
+            />
+            <Product
+              id={product_id}
+              title={
+                "D-Link Wireless AC Smartbeam 1750 Mbps Home Cloud App-Enabled Dual-Band Gigabit Router (DIR-868L)"
+              }
+              price={20}
+              rating={3}
+              image={
+                "https://m.media-amazon.com/images/I/41MGx8BGC5L._AC_UY218_.jpg"
+              }
+            />
           </div>
           <div className="home__row">
-            <Product />
+            <Product
+              id={product_id}
+              title={
+                "D-Link Wireless AC Smartbeam 1750 Mbps Home Cloud App-Enabled Dual-Band Gigabit Router (DIR-868L)"
+              }
+              price={1199}
+              rating={5}
+              image={
+                "https://m.media-amazon.com/images/I/61QSUZYl+dL._AC_UY218_.jpg"
+              }
+            />
           </div>
         </div>
       </div>
--- src/components/product/product.component.jsx
+++ src/components/product/product.component.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import "./product.styles.css";
 
-const Product = () => {
+const Product = ({ id, title, price, rating, image }) => {
   return (
     <div className="product">
-      <div className="product__info">
-        <p>
-          10 inch Tablet Android 13 Tablets, 6(2+4) GB + 64GB ROM 512GB Expand,
-          Quad-Core Processor, 1280x800 IPS Screen, GPS, WiFi, Dual Camera,
-          Bluetooth, 6000mAh Battery{" "}
-        </p>
+      <div className="product__info" id={id}>
+        <p>{title}</p>
         <p className="product__price">
-          <small></small>
-          <strong>$30</strong>
+          <strong>${price}</strong>
         </p>
-        <div className="product__rating">⭐⭐⭐⭐</div>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p key={i}>⭐</p>
+            ))}
+        </div>
       </div>
-      <img
-        src="https://m.media-amazon.com/images/I/410m0InPF4L._MCnd_AC_.jpg"
-        alt="tablet"
-      />
+      <img src={image} alt={title} />
       <button>Add to basket</button>
     </div>
   );
